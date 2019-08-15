0c46d6669fc84cb5ae54422fc2ddf1659ede4d92
Debbie <debbieforbeck@gmail.com>
2019-08-15 15:57:47 -0400

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================                                 
+-- Author      : Debbie                                  
+-- Create date : 2019-08-15                                
+-- Description : To Create Database                           
+--   source: 
+-- https://code.msdn.microsoft.com/ASPNET-MVC-5-Security-And-44cbdb97   
+-- =============================================   
+-- CREATES SAMPLE DATABASE THAT HOUSES BOTH USER AND DATA VALUES
+
+--Script to create DB,Table and sample Insert data   
+USE MASTER;   
+-- 1) Check for the Database Exists .If the database is exist then drop and create new DB   
+IF EXISTS (SELECT [name] FROM sys.databases WHERE [name] = 'AttendanceDB' )   
+BEGIN   
+ALTER DATABASE AttendanceDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE   
+DROP DATABASE AttendanceDB ;   
+END     
+   
+CREATE DATABASE AttendanceDB   
+GO   
+   
+USE AttendanceDB 
